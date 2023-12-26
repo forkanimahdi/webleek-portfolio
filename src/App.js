debeb7aed2818a8e43a47bd1aec417600394c913
forkanimahdi <forkanimahdi@gmail.com>
2023-12-26 11:52:53 +0100

    Folder Structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Error } from "./Error";
+import { Home } from "./pages/home/Home";
+import { About } from "./pages/about/About";
+// import { Navigation } from "./layouts/Navigation";
+
+export const App = () => {
+  return (
+    <>
+      {/* <Navigation /> */}
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+};
---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Error = () => {
+  return (
+    <>
+    <div className="d-flex align-items-center justify-content-center w-100 h-100 bg-dark text-danger">
+          <h1 className="">Error 404 , Page Not Found !</h1>
+    </div>
+
+    </>
+  );
+};
